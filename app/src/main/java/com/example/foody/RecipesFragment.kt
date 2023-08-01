4143ca9a0a5c01cacebcacc9ce3c737fe9ff1838
Bullets-jyr <jyr@bullets.co.kr>
2023-08-01 22:16:03 +0900

    12. Design Recyclerview Placeholder Layout

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foody.databinding.FragmentRecipesBinding
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
+    lateinit var binding: FragmentRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        binding = FragmentRecipesBinding.inflate(inflater)
+
+        binding.recyclerView.showShimmer()
+
+        return binding.root
     }
 }
