b218b2fa039c6a93a83d4cb81e352c205e0a887a
Bullets-jyr <jyr@bullets.co.kr>
2023-08-11 19:05:28 +0900

    29. Create and Inject MainViewModel class

--- app/src/main/java/com/example/foody/util/NetworkResult.kt
+++ app/src/main/java/com/example/foody/util/NetworkResult.kt
@@ -6,6 +6,6 @@ sealed class NetworkResult<T>(
 ) {
 
     class Success<T>(data: T): NetworkResult<T>(data)
-    class Error<T>(message: String?, data: T?): NetworkResult<T>(data, message)
+    class Error<T>(message: String?, data: T? = null): NetworkResult<T>(data, message)
     class Loading<T>: NetworkResult<T>()
 }
