c2b6a101bc41654859b3d3d42f9f198157ff0819
Bullets-jyr <jyr@bullets.co.kr>
2023-08-25 16:39:25 +0900

    36. Create RecipesViewModel class

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -5,25 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foody.MainViewModel
+import com.example.foody.viewmodels.MainViewModel
 import com.example.foody.adapter.RecipesAdapter
 import com.example.foody.databinding.FragmentRecipesBinding
-import com.example.foody.util.Constants.Companion.API_KEY
 import com.example.foody.util.NetworkResult
-import dagger.hilt.android.AndroidEntryPoint
+import com.example.foody.viewmodels.RecipesViewModel
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 //@AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
     private val mAdapter by lazy { RecipesAdapter() }
     lateinit var binding: FragmentRecipesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +37,6 @@ class RecipesFragment : Fragment() {
         binding = FragmentRecipesBinding.inflate(inflater, container, false)
 
 //        binding.recyclerView.showShimmer()
-        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
 
         setupRecyclerView()
         requestApiData()
@@ -42,7 +45,7 @@ class RecipesFragment : Fragment() {
     }
 
     private fun requestApiData() {
-        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
         mainViewModel.recipesResponse.observe(viewLifecycleOwner, { response ->
             when (response) {
                 is NetworkResult.Success -> {
@@ -66,18 +69,18 @@ class RecipesFragment : Fragment() {
         })
     }
 
-    private fun applyQueries(): HashMap<String, String> {
-        val queries: HashMap<String, String> = HashMap()
-
-        queries["number"] = "50"
-        queries["apiKey"] = API_KEY
-        queries["type"] = "snack"
-        queries["diet"] = "vegan"
-        queries["addRecipeInformation"] = "true"
-        queries["fillIngredients"] = "true"
-
-        return queries
-    }
+//    private fun applyQueries(): HashMap<String, String> {
+//        val queries: HashMap<String, String> = HashMap()
+//
+//        queries["number"] = "50"
+//        queries["apiKey"] = API_KEY
+//        queries["type"] = "snack"
+//        queries["diet"] = "vegan"
+//        queries["addRecipeInformation"] = "true"
+//        queries["fillIngredients"] = "true"
+//
+//        return queries
+//    }
 
     private fun setupRecyclerView() {
         binding.recyclerView.adapter = mAdapter
--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -5,7 +5,14 @@ class Constants {
     companion object {
 
         const val BASE_URL = "https://api.spoonacular.com"
-//        const val API_KEY = "ef9c6b659ab44f799c6efb9cb010ee76"
-        const val API_KEY = "e8976bda2a834369a10bab7e0c530211"
+        const val API_KEY = "[YOUR API KEY HERE]"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
     }
 }
