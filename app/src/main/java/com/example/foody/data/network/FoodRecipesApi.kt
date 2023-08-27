4ab1eaaff51096b7c44db125b16300d574d817c7
Bullets-jyr <jyr@bullets.co.kr>
2023-08-27 16:22:04 +0900

    68. FoodRecipesApi - searchRecipes()

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -12,4 +12,9 @@ interface FoodRecipesApi {
     suspend fun getRecipes(
         @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap searchQuery: Map<String, String>
+    ): Response<FoodRecipe>
 }
