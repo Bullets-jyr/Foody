850672ed50dcc100c19f522e76b376f9e53b8cbe
Bullets-jyr <jyr@bullets.co.kr>
2023-08-27 15:44:01 +0900

    66. Back Online - Persist data with DataStore

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -60,7 +60,11 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
 //        requestApiData()
-        readDatabase()
+//        readDatabase()
+
+        recipesViewModel.readBackOnline.observe(viewLifecycleOwner, {
+            recipesViewModel.backOnline = it
+        })
 
         lifecycleScope.launch {
             networkListener = NetworkListener()
@@ -69,7 +73,7 @@ class RecipesFragment : Fragment() {
                     Log.d("NetworkListener", status.toString())
                     recipesViewModel.networkStatus = status
                     recipesViewModel.showNetworkStatus()
-//                    readDatabase()
+                    readDatabase()
                 }
         }
 
