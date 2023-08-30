2d27141df53a159815371e42abb8d0e546c78e87
Bullets-jyr <jyr@bullets.co.kr>
2023-08-30 16:05:04 +0900

    106. Close Contextual Action Mode

--- app/src/main/java/com/example/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -49,5 +49,6 @@ class FavoriteRecipesFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        mAdapter.clearContextualActionMode()
     }
 }
